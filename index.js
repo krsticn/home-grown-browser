57b45137ad58ec01d5531360d9984e9174e03e36
Nikola Krstic <krsticn94@gmail.com>
2026-02-20 21:28:07 +0100

    Create custom url parser

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log("Hello World");
+class CustomURL {
+  constructor(url)  {
+      if (!url.includes("://")) {
+        throw new Error('Invalid URL format');
+      }
+
+      let [schema, restUrl = ""] = url.split("://");
+
+      if (schema !== 'http') {
+        throw new Error('This browser supports only http protocol');
+      }
+
+      if (!restUrl) {
+         throw new Error("Missing host");
+      }
+
+      if (!restUrl.includes('/')) {
+         restUrl += '/';
+      } 
+
+      const slashIndex = restUrl.indexOf('/');
+
+      this.schema = schema;
+      this.host = restUrl.slice(0, slashIndex);
+      this.path = restUrl.slice(slashIndex);
+  }
+}
+
+const parsedUrl = new CustomURL("http://example.com/foo/bar/bazz");
+
+console.log(parsedUrl.schema);
+console.log(parsedUrl.host);
+console.log(parsedUrl.path);
