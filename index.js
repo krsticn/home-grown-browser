5e3e725e3d9263c678f6a911f044e4247c6ee184
Nikola Krstic <krsticn94@gmail.com>
2026-02-21 16:08:06 +0100

    Add logger

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+import { Logger } from './lib/logger.js';
 import { CustomURL } from './lib/custom-url.js';
+
+global.logger = new Logger();
 
 const parsedUrl = new CustomURL("http://example.com/");
 
