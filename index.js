4e06905a508a5f5adf2d554d10d3e161926b306b
Nikola Krstic <krsticn94@gmail.com>
2026-02-21 11:10:27 +0100

    Add socket connection

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { Socket } from 'net';
+
 class CustomURL {
   constructor(url)  {
       if (!url.includes("://")) {
@@ -20,14 +22,41 @@ class CustomURL {
 
       const slashIndex = restUrl.indexOf('/');
 
+      this.url = url;
       this.schema = schema;
       this.host = restUrl.slice(0, slashIndex);
       this.path = restUrl.slice(slashIndex);
   }
+
+  request() {
+      const socket = new Socket();
+
+      socket.connect(80, this.host, () => {
+        console.log('Connected to TCP server');
+        socket.write(`GET ${this.path} HTTP/1.0\r\nHost: ${this.host}\r\n\r\n`, 'utf-8');
+      })
+
+      socket.on('data', (data) => {
+        console.log(this.getTextFromBuffer(data));
+      })
+
+      socket.on('error', (error) => {
+        console.log('Error while connecting to TCP server', error);
+      })
+
+      socket.on('close', () => {
+        console.log('Connection to TCP server is closed');
+      })
+    }
+
+    getTextFromBuffer(arrayBuffer) {
+      const uInt8Array = new Uint8Array(arrayBuffer);
+      const decoder = new TextDecoder('utf-8');
+      return decoder.decode(uInt8Array);
+    }
 }
 
-const parsedUrl = new CustomURL("http://example.com/foo/bar/bazz");
+const parsedUrl = new CustomURL("http://example.com/");
 
-console.log(parsedUrl.schema);
-console.log(parsedUrl.host);
-console.log(parsedUrl.path);
+parsedUrl.request();
+
