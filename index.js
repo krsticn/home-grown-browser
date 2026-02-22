f58332a3296393db4529d1a086855cfab6744032
Nikola Krstic <krsticn94@gmail.com>
2026-02-22 17:11:28 +0100

    Request user input url

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 import { Logger } from './lib/logger.js';
-import { CustomURL } from './lib/custom-url.js';
+import { ParseURL } from './lib/parse-url.js';
+
+const argUrl = process.argv[2];
+
+if (!argUrl) {
+  console.log("Please provide url");
+  process.exit(1);
+}
 
 global.logger = new Logger();
 
-const parsedUrl = new CustomURL("http://example.com/");
+function main() {
+  try {
+    const parsedUrl = new ParseURL(argUrl);
+    parsedUrl.request();
+  } catch(err) {
+    console.error(err);
+  }
+}
 
-parsedUrl.request();
-
+main();
