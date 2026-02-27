9aa1bb8aaa9149185cc254ffc2d4962b2d3ecd6e
Nikola Krstic <krsticn94@gmail.com>
2026-02-27 08:54:25 +0100

    Refactor http client

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 import { ParseURL } from './lib/parse-url.js';
+import fetchRaw  from './lib/fetch-raw.js';
 
-const argUrl = process.argv[2];
+const url = process.argv[2];
 
-if (!argUrl) {
+if (!url) {
   console.log("Please provide url");
   process.exit(1);
 }
 
-function main() {
+async function main() {
   try {
-    const parsedUrl = new ParseURL(argUrl);
-    parsedUrl.request();
+    const parsedUrl = new ParseURL(url);
+
+    const res = await fetchRaw({
+      port: parsedUrl.port,
+      host: parsedUrl.host,
+      path: parsedUrl.path,
+      schema:  parsedUrl.schema,
+    })
+
+    console.log(res);
   } catch(err) {
     console.error(err);
   }
